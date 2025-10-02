20c69473c7ca5f137e061e7f4dc48690a156e3e9
Vitor Moura <jvrezendemoura@gmail.com>
2025-10-02 14:11:17 -0300

    feature: Creating the endpoint for authentication backend and creating the new endpoints for the user detail view and adding the frontend for the project

--- backend/api/backend.py
+++ backend/api/backend.py
@@ -1,14 +1,25 @@
 from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.models import User 
+from django.contrib.auth import get_user_model 
 from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
+
 class EmailBackend(BaseBackend):
     """
         Authetication system based on the user email
         for the login and the register system be based on 
         the unique field inside of the database
     """
-    def authenticate(self, request, email=None, password=None, token=None):
-        pass
+    def authenticate(self, request, email=None, password=None):
+        try:
+            user = User.objects.get(email = email)
+            
+            password_check = check_password(user.password, password)
+            if password_check:
+                return user             
+
+        except User.DoesNotExist:
+            return None            
     
     def get_user(self, user_id):
         try:
--- backend/api/tests/unit/test_views.py
+++ backend/api/tests/unit/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 import inspect 
 import importlib 
 from rest_framework import views
-from api.serializers import VideoSerializer
+from api.serializers import VideoSerializer, UserGetSerializer
 
 class TestViews(TestCase):
     def setUp(self) -> None:
@@ -107,3 +107,54 @@ class TestViews(TestCase):
         class_ = module.VideoView
         signature = inspect.signature(class_.post)
         self.assertTrue("request" in signature.parameters.keys())
+
+
+class TestUserViews(TestCase):
+    def setUp(self) -> None:
+        pass 
+    
+    def test_if_user_detail_view_exists(self) -> None:
+        module = importlib.import_module("api.views")
+        self.assertTrue(hasattr(module, "UserDetailView"))
+        
+    def test_if_user_detail_view_is_usable(self) -> None:
+        module = importlib.import_module("api.views")
+        class_ = module.UserDetailView
+        self.assertTrue(callable(class_))
+        
+    def test_if_user_detail_view_is_the_correct_sub_class(self) -> None:
+        module = importlib.import_module("api.views")
+        class_ = module.UserDetailView
+        self.assertTrue(issubclass(class_, views.APIView))
+        
+    def test_if_user_detail_view_have_serializer_class_variable(self) -> None:
+        module = importlib.import_module("api.views")
+        class_ = module.UserDetailView
+        self.assertTrue(hasattr(class_, "serializer_class"))
+        
+    def test_if_user_detail_view_serializer_class_is_correct(self) -> None:
+        module = importlib.import_module("api.views")
+        class_ = module.UserDetailView
+        self.assertIs(class_.serializer_class, UserGetSerializer)
+        self.assertTrue(True) # Modificar
+        
+        
+    def test_if_user_detail_view_have_get_method(self) -> None:
+        module = importlib.import_module("api.views")
+        class_ = module.UserDetailView
+        self.assertTrue(hasattr(class_, "get"))
+        
+    def test_if_get_request_have_correct_method_signature(self) -> None:
+        required_fields = ["request","pk","format","self"]
+        module = importlib.import_module("api.views")
+        class_ = module.UserDetailView
+        signature = inspect.signature(class_.get)
+        
+        for keys in signature.parameters.keys():
+            if keys in required_fields:
+                required_fields.remove(keys)
+
+        self.assertListEqual(required_fields, [])
+            
+    # TODO: Fazer os testes necessarios da classe para prevenir os problemas,
+    # tipo post, proibido de fazer        
