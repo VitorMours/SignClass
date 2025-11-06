2b89cb83853f02fecf2a8d5f36c79be852228561
Lucas Santos Nascimento <luccassantos2012@bol.com.br>
2025-11-06 19:51:52 -0300

    feat: Adiciona testes de integração para cadastro de usuário (signup)

--- backend/api/tests/unit/test_urls.py
+++ backend/api/tests/unit/test_urls.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 import importlib
+from django.contrib.auth import get_user_model
 
 class TestAuthUrls(TestCase):
     def setUp(self) -> None:
@@ -9,16 +10,16 @@ class TestAuthUrls(TestCase):
         self.assertTrue(True)
         
     def test_if_auth_token_route_exists(self) -> None:
-        response = self.client.post("/api/auth/login")
-        self.assertEqual(response.status_code, 400)
+        response = self.client.post("/api/auth/login", data={})
+        self.assertEqual(response.status_code, 400) 
         
-    def test_if_auth_token_route_returns_not_found(self) -> None:
-        response = self.client.get("/api/auth/login/")
-        self.assertEqual(response.status_code, 404)
+    def test_if_auth_token_route_blocks_get_method(self) -> None:
+        response = self.client.get("/api/auth/login")
+        self.assertEqual(response.status_code, 405)
         
     def test_if_auth_token_return_200_sucess_in_message(self) -> None:
-        #response = self.client.get|()
         pass
+
 class TestUserUrls(TestCase):
     def setUp(self) -> None:
         self.client = Client()
@@ -52,3 +53,38 @@ class TestUserUrls(TestCase):
                 exists = True
                 break
         self.assertTrue(exists)
+
+    def test_se_o_cadastro_de_usuario_funciona(self) -> None:
+        user_data = {
+            "email": "usuario@teste.com",
+            "password": "SenhaForte@123",
+            "first_name": "Tester",
+            "last_name": "QA"
+        }
+        user_count_before = get_user_model().objects.count()
+        response = self.client.post("/api/users", data=user_data)
+        
+        self.assertEqual(response.status_code, 201)
+        user_count_after = get_user_model().objects.count()
+        self.assertEqual(user_count_after, user_count_before + 1)
+        
+        response_data = response.json()
+        self.assertEqual(response_data['user']['email'], user_data['email'])
+
+    def test_se_cadastro_falha_com_email_duplicado(self) -> None:
+        get_user_model().objects.create_user(
+            email="email.duplicado@teste.com",
+            password="123",
+            first_name="Usuario Original"
+        )
+        
+        user_data_duplicada = {
+            "email": "email.duplicado@teste.com",
+            "password": "OutraSenha@123",
+            "first_name": "Tentativa de Clone"
+        }
+        user_count_before = get_user_model().objects.count()
+        response = self.client.post("/api/users", data=user_data_duplicada)
+        self.assertEqual(response.status_code, 400)
+        user_count_after = get_user_model().objects.count()
+        self.assertEqual(user_count_after, user_count_before)
